27c0a7454d00999fd159a1237053e665dac30eba
L1oid <71085120+L1oid@users.noreply.github.com>
2022-02-16 11:17:00 +0700

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
 echo off
 
-"C:\Program Files\CodeBlocks\MinGW\bin\g++" Server.cpp IServer.cpp Main.cpp -o run.exe
+"C:\Program Files\CodeBlocks\MinGW\bin\g++" Server.cpp IServer.cpp Main.cpp -c 
+C:\Program Files\CodeBlocks\MinGW\bin\g++" Server.o IServer.o Main.o run.exe -static-libstdc++ -static-libgcc
 
 PAUSE
