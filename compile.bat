aac5fb4a130114f0d08c3ae90543295882172c23
L1oid <lloidtm@gmail.com>
2022-10-05 14:43:25 +0700

    Upd Repos

--- compile.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-g++ -c ./source/Server.cpp -o ./build/Server.o
-g++ -c ./source/Server2.cpp -o ./build/Server2.o
-g++ -c ./source/IManager.cpp -o ./build/IManager.o
-g++ -c ./source/Main.cpp -o ./build/Main.o
-
-g++ ./build/Server.o ./build/Server2.o ./build/IManager.o ./build/Main.o -o ./build/run.exe
